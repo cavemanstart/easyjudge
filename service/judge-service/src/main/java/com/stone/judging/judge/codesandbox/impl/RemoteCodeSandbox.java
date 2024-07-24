9f3d596de7300776b3579a82a4c206a77fdc0723
stone <1265286368@qq.com>
2024-07-24 11:05:28 +0800

    xx

--- service/judge-service/src/main/java/com/stone/judging/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ service/judge-service/src/main/java/com/stone/judging/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -7,24 +7,27 @@ import com.stone.common.exception.BusinessException;
 import com.stone.judging.judge.codesandbox.CodeSandbox;
 import com.stone.model.codesandbox.ExecuteCodeRequest;
 import com.stone.model.codesandbox.ExecuteCodeResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * 远程代码沙箱（实际调用接口的沙箱）
  */
+@Slf4j
 public class RemoteCodeSandbox implements CodeSandbox {
 
     // 定义鉴权请求头和密钥
     private static final String AUTH_REQUEST_HEADER = "auth";
 
     private static final String AUTH_REQUEST_SECRET = "secretKey";
-
-
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        String url = "http://localhost:8090/executeCode";
+        String url = "http://192.168.80.142:8198/executeCode";
         String json = JSONUtil.toJsonStr(executeCodeRequest);
+        log.info("代码沙箱请求地址：" + url);
         String responseStr = HttpUtil.createPost(url)
                 .header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET)
                 .body(json)
